1cda5bd7bc067f874952991d6a997079877868ec
Yurii <gapirov21@gmail.com>
2023-09-02 22:31:35 +0300

    hw7-task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const container = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems)
+let instance;
+container.insertAdjacentHTML('beforeend', markup);   //виводимо на живу сторінку контейнер
+container.addEventListener('click', handleGalleryClick);
+
+function createMarkup (arr) {
+    return arr.map(({preview, original, description }) => {
+        return `<li data-preview="${preview}" class="gallery__item js-gallery-item">
+        <a class="gallery__link" href="${original}">
+        <img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/>
+        </a>
+        </li>`
+
+    }).join('');
+}
+
+function handleGalleryClick (evt) {
+    evt.preventDefault();   //блокуєму перехід за посиланням при кліку
+    if(evt.target === evt.currentTarget) {
+        return;
+    }
+
+    const targetElement = evt.target.closest('.js-gallery-item');
+    const galleryDesc = targetElement.dataset.preview;
+    const galleryInfo = galleryItems.find(gallery => gallery.preview === galleryDesc)
+  
+    
+
+   
+const instance = basicLightbox.create(`
+<div class="modal">
+<img src="${galleryInfo.original}" alt="${galleryInfo.description}"/>
+</div>
+`);
+
+instance.show();
+
+};
 console.log(galleryItems);
+
+
+// закриття модалки ESC
+
+
+
+
